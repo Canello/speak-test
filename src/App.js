46c44483c3d9f821d7ca1f62be6d1b6878727b63
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-06-27 10:36:32 -0300

    c

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import EasySpeech from "easy-speech";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const init = async () => {
+        await EasySpeech.init({ maxTimeout: 5000, interval: 250 })
+            .then(() => console.log("load complete"))
+            .catch((e) => console.error(e));
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    const [text, setText] = useState("");
+
+    const speak = async (event) => {
+        const voice = EasySpeech.voices().filter(
+            (voice) => voice.lang === "en-US"
+        )[0];
+
+        await EasySpeech.speak({
+            text: text,
+            voice: voice, // optional, will use a default or fallback
+            pitch: 1,
+            rate: 0.8,
+            volume: 1,
+            // there are more events, see the API for supported events
+            boundary: (e) => console.debug("boundary reached"),
+        });
+    };
+
+    return (
+        <div className="App">
+            <input
+                value={text}
+                onChange={(event) => setText(event.target.value)}
+            />
+            <button onClick={speak}>Speak</button>
+            <button onClick={() => console.log(EasySpeech.status())}>
+                Status
+            </button>
+        </div>
+    );
 }
 
 export default App;
